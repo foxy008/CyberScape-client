481467be406f499c1a43567ac358e4fe2849df63
--global <--global>
2023-05-06 22:03:47 +0700

    Feat: Share Twitter and Facebook Buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 import GalaxyBackground from "../components/GalaxyBackground";
+import ShareButtons from "../components/ShareButtons";
 
 export default function Home() {
-  return <GalaxyBackground></GalaxyBackground>;
+  return (
+    <GalaxyBackground>
+      <ShareButtons title="Ini NFT 1" url="https://google.com/nft1" />
+    </GalaxyBackground>
+  );
 }
