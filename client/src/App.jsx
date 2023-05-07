0697935e009bc366674446a3dcae312c043f4272
Satrio Prabowo <satrioprabowo47@gmail.com>
2023-05-07 11:32:03 +0700

    add: gallery route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
-import HomePage from "./pages/Home";
+import Gallery from "./pages/Gallery";
+// import HomePage from "./pages/Home";
 
 function App() {
   return (
-    <RouterProvider router={router}/>
-    // <HomePage />
+    <RouterProvider router={router}>
+      <Gallery />
+    </RouterProvider>
   );
 }
 
--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -1,13 +1,14 @@
 
 import { createBrowserRouter } from 'react-router-dom'
-import Homepage from '../pages/Home'
+// import Homepage from '../pages/Home'
 
 import Layout from '../components/Layout'
+import Gallery from '../pages/Gallery'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Homepage />
+    element: <Gallery />
   }
 ])
 
