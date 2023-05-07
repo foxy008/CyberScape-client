eea78f781a30c114e6a1d98559a70d5dcd43574d
Satrio Prabowo <satrioprabowo47@gmail.com>
2023-05-07 15:59:57 +0700

    chore: edit NFT twitter message

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import ShareButtons from "../components/ShareButtons";
 export default function Home() {
   return (
     <GalaxyBackground>
-      <ShareButtons title="Ini NFT 1" url="https://google.com/nft1" />
+      <ShareButtons title="Just discovered this amazing NFT on CyberScape by [artist name]. I'm in love with this unique and creative artwork! Check it out and add it to your collection today. #NFT #cryptoart #digitalart #blockchainart [insert NFT link or image]" url="https://google.com/nft1" />
     </GalaxyBackground>
   );
 }
