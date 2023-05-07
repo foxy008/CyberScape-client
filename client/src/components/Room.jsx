78b4ba50cc6e1f2765df37e2c67113b0fc471795
satrioprbw <satrioprabowo47@gmail.com>
2023-05-08 00:11:35 +0700

    chore: add logo and rooms update

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,33 +1,55 @@
 
-export default function Room({ position }) {
+export default function Room({ position, rotation, nfts }) {
+  console.log(nfts?.roomNFTs);
   return (
     <>
-      <a-entity position={position}>
-        <a-box src="#wallTexture" position="0 1 -5" width="10" height="5" depth="0.1">
-            <a-plane position="0 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-            <a-plane position="-3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1VemkxUkpNY0RWeERGUkY4WVVIclc3elpieEZHQUdBeWFLZEU0bUg4M1BERA==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-            <a-plane position="3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="1.5" height="2"></a-plane>
+      <a-entity position={position} rotation={rotation}>
+        <a-box src="#wallTexture" position="0 3.5 -8" width="22" height="7" depth="0.1"></a-box>
+        <a-box src="#wallTexture" position="0 3.5 8" width="22" height="7" depth="0.1"></a-box>
+        <a-box material="opacity: 0.5" rotation="0 90 0" position="-11 3.5 0" width="16" height="7" depth="0.1"></a-box>
+        
+        <a-box color="black" rotation="0 90 0" position="11 2.5 0" width="4" height="5" depth="0.1">
+          <a-plane src="/assets/cyberscape-logo-black.png" position="0 0 0.08" width="4" height="5">
+
+          </a-plane>
+        </a-box>
+        <a-box src="#wallTexture" position="0 7 0" width="22" height="16" depth="0.1" rotation="90 0 0">
+        </a-box>
+        <a-box src="" position="0 2.5 -7.5" width="13" height="4" depth="0.1">
+          <a-plane position="0 0 0.1" src='' rotation="0 0 0" width="2.4" height="3"></a-plane>
+          <a-plane position="-4 0 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1VemkxUkpNY0RWeERGUkY4WVVIclc3elpieEZHQUdBeWFLZEU0bUg4M1BERA==" rotation="0 0 0" width="2.4" height="3"></a-plane>
+          <a-plane position="4 0 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="2.4" height="3"></a-plane>
           <a-light type="point" color="blue" position="0 0.5 1" intensity="0.1"></a-light>
           <a-light type="point" color="red" position="3 0.5 1" intensity="0.05"></a-light>
-          <a-light type="point" color="blue" position="-3 0.5 1" intensity="0.1"></a-light>
+          <a-light type="point" color="blue" position="-3 0.5 1" intensity="0.4"></a-light>
+          <a-cylinder position="6 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="-6 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="2 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="-2 -2 0" color="black" radius="0.04"></a-cylinder>
         </a-box>
-        <a-box src="#wallTexture" position="0 1 5" width="10" height="5" depth="0.1" rotation="0 180 0">
-          <a-plane position="0 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-          <a-plane position="-3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1VemkxUkpNY0RWeERGUkY4WVVIclc3elpieEZHQUdBeWFLZEU0bUg4M1BERA==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-          <a-plane position="3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="1.5" height="2"></a-plane>
+        <a-box src="" position="0 2.5 7.5" width="13" height="4" depth="0.1" rotation="0 180 0">
+          <a-plane position="0 0 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="2.4" height="3"></a-plane>
+          <a-plane position="-4 0 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1VemkxUkpNY0RWeERGUkY4WVVIclc3elpieEZHQUdBeWFLZEU0bUg4M1BERA==" rotation="0 0 0" width="2.4" height="3"></a-plane>
+          <a-plane position="4 0 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="2.4" height="3"></a-plane>
+          <a-cylinder position="6 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="-6 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="2 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="-2 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-light type="ambient" color="white" position="0 1 1" intensity="0.1"></a-light>
+        </a-box>
+        <a-box position="5 2.5 0" width="5" height="4" depth="0.1" rotation="0 -135 0">
+          <a-plane position="0 0 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="2.4" height="3"></a-plane>
+          <a-plane position="0 0 -0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 180 0" width="2.4" height="3"></a-plane>
+          <a-cylinder position="2 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="-2 -2 0" color="black" radius="0.04"></a-cylinder>
+        </a-box>
+        <a-box src="" position="-5 2.5 0" width="5" height="4" depth="0.1" rotation="0 -135 0">
+          <a-plane position="0 0 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="2.4" height="3"></a-plane>
+          <a-plane position="0 0 -0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 180 0" width="2.4" height="3"></a-plane>
+          <a-cylinder position="2 -2 0" color="black" radius="0.04"></a-cylinder>
+          <a-cylinder position="-2 -2 0" color="black" radius="0.04"></a-cylinder>
+        </a-box>
 
-          <a-light type="ambient" color="white" position="0 1 1" intensity="0.01"></a-light>
-        </a-box>
-        <a-box src="#wallTexture" position="5 1 0" width="5" height="5" depth="0.1" rotation="0 -90 0">
-          <a-plane position="-1.5 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-          <a-plane position="1.5 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-        </a-box>
-        <a-box src="#wallTexture" position="-5 1 0" width="5" height="5" depth="0.1" rotation="0 90 0">
-          <a-plane position="-1.5 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-          <a-plane position="1.5 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-        </a-box>
-        <a-box color="white" position="0 3.5 0" width="10" height="10" depth="0.1" rotation="90 0 0">
-        </a-box>
 
       </a-entity>
     </>
--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -19,11 +19,12 @@ export default function Gallery() {
   const elevatorRef = useRef(null);
 
   function handleElevatorClick() {
-    setCameraY(3.0);
+    setCameraY(9.6);
   }
+
   return <>
     {
-      !rooms ? null : console.log(rooms)
+      !rooms ? null : console.log(rooms[2]?.RoomNFTs[0].NFT.imageUrl)
       // Buat cek apakah rooms dapet di fetch, uncomment kalo ngak mau console
     }
     <a-scene>
@@ -38,44 +39,45 @@ export default function Gallery() {
           id="wallTexture"
           src="/assets/output.jpg" />
       </a-assets>
-      <a-camera position={`-20 ${cameraY} 20`} >
+      <a-camera position={`22 ${cameraY} 16`} >
         <a-cursor raycaster="objects: .clickable"></a-cursor>
       </a-camera>
-      <a-sphere
-        className="clickable"
-        ref={elevatorRef}
-        position="5 2 25"
-        width="0.3"
-        height="1"
-        scale="0.1 0.1 0.1"
-        events={{ click: handleElevatorClick }}
-      ></a-sphere>
-      <a-box position="5 0 0" width="0.5" height="" depth="3"></a-box>
-      <a-box position="5 0 10" width="0.5" height="" depth="3"></a-box>
-      <a-box position="5 0 20" width="0.5" height="" depth="3"></a-box>
-      <a-box position="5 0 30" width="0.5" height="" depth="3"></a-box>
-      <a-box position="5 0 40" width="0.5" height="" depth="3"></a-box>
-      <Room position="-2 0 0" />
-      <Room position="12 0 0" />
-      <Room position="-2 0 10" />
-      <Room position="12 0 10" />
-      <Room position="-2 0 20" />
-      <Room position="12 0 20" />
-      <Room position="-2 0 30" />
-      <Room position="12 0 30" />
-      <Room position="-2 0 40" />
-      <Room position="12 0 40" />
-      <Room position="-2 5 0" />
-      <Room position="12 5 0" />
-      <Room position="-2 5 10" />
-      <Room position="12 5 10" />
-      <Room position="-2 5 20" />
-      <Room position="12 5 20" />
-      <Room position="-2 5 30" />
-      <Room position="12 5 30" />
-      <Room position="-2 5 40" />
-      <Room position="12 5 40" />
-      <a-entity environment="preset: tron; dressingAmount: 50; seed: 9; playArea: 4"></a-entity>
+      <a-entity id="mouseCursor" cursor="rayOrigin: mouse"></a-entity>
+
+      <a-entity rotation="0 0 0" animation="property: rotation; to: 0 360 0; loop: true; dur: 10000">
+        <a-sphere position="18 9 0" color="mediumseagreen"></a-sphere>
+</a-entity>
+    
+      <a-box material="opacity: 0.5" rotation="0 90 0" position="23 7 16" width="80" height="14" depth="0.1"></a-box>
+      <a-box material="opacity: 0.5" rotation="0 90 0" position="15 7.5 37" width="38" height="1" depth="0.1"></a-box>
+      <a-box material="opacity: 0.5" rotation="0 90 0" position="15 7.5 -5" width="38" height="1" depth="0.1"></a-box>
+      <a-box material="opacity: 0.5" rotation="0 0 0" position="17 7 -24" width="12" height="14" depth="0.1"></a-box>
+      <a-box material="opacity: 0.5" rotation="0 0 0" position="17 7 56" width="12" height="14" depth="0.1"></a-box>
+      <a-box material="opacity: 0.9" rotation="0 0 0" position="21.5 7 18" width="3" height="14" depth="0.1"></a-box>
+      <a-box material="opacity: 0.5" rotation="0 0 0" position="17.5 7.5 18" width="5" height="1" depth="0.1"></a-box>
+      <a-box material="opacity: 0.5" rotation="0 0 0" position="17.5 7.5 14" width="5" height="1" depth="0.1"></a-box>
+      <a-box material="opacity: 0.9" rotation="0 0 0" position="21.5 7 14" width="3" height="14" depth="0.1"></a-box>
+      <a-box animation="property: position; to: 20 1 14.5; dur: 2000; easing: linear; startEvents: mouseenter;" material="opacity: 1" rotation="0 90 0" position="20 1 15.5" width="1" height="3" depth="0.1"></a-box>
+      <a-box animation="property: position; to: 20 1 17.5; dur: 2000; easing: linear; startEvents: mouseenter;" material="opacity: 1" rotation="0 90 0" position="20 1 16.5" width="1" height="3" depth="0.1"></a-box>
+      <a-box src="#wallTexture" rotation="90 0 90" position="18 14 16" width="84" height="14" depth="0.1"></a-box>
+      <a-box src="#wallTexture" rotation="90 0 90" position="13 7 16" width="80" height="4" depth="0.1"></a-box>
+      <a-box src="#wallTexture" rotation="90 0 90" position="17.5 7 16" width="4" height="5" depth="0.1"></a-box>
+
+      <a-box position="18 0 0" width="0.5" height="" depth="3"></a-box>
+      <a-box position="18 0 32" width="0.5" height="" depth="3"></a-box>
+      <a-box position="18 0 48" width="0.5" height="" depth="3"></a-box>
+      <a-box position="18 0 -16" width="0.5" height="" depth="3"></a-box>
+      <Room position="0 0 -16" nfts={rooms[2]}/>
+      <Room position="0 0 0" />
+      <Room position="0 0 16" />
+      <Room position="0 0 32" />
+      <Room position="0 0 48" />
+      <Room position="0 7 -16" />
+      <Room position="0 7 0" />
+      <Room position="0 7 16" />
+      <Room position="0 7 32" />
+      <Room position="0 7 48" />
+      <a-entity environment="preset: tron; fog: 0.8; dressingAmount: 50; seed: 300; playArea: 5"></a-entity>
     </a-scene>
 
   </>
