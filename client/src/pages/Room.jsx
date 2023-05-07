797117a4ce35468b195efcdd889ac6c2baa03c33
Satrio Prabowo <satrioprabowo47@gmail.com>
2023-05-07 15:53:16 +0700

    chore: edit room position

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -4,9 +4,9 @@ export default function Room({ position }) {
     <>
       <a-entity position={position}>
         <a-box src="#wallTexture" position="0 1 -5" width="10" height="5" depth="0.1">
-          <a-plane position="0 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-          <a-plane position="-3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1VemkxUkpNY0RWeERGUkY4WVVIclc3elpieEZHQUdBeWFLZEU0bUg4M1BERA==" rotation="0 0 0" width="1.5" height="2"></a-plane>
-          <a-plane position="3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="1.5" height="2"></a-plane>
+            <a-plane position="0 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1hU3QxbnZTcTh5Wjd1VmpMZFFvZzhvTVd0RG56djNRS1FidmtKTlppRDk0aQ==" rotation="0 0 0" width="1.5" height="2"></a-plane>
+            <a-plane position="-3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1VemkxUkpNY0RWeERGUkY4WVVIclc3elpieEZHQUdBeWFLZEU0bUg4M1BERA==" rotation="0 0 0" width="1.5" height="2"></a-plane>
+            <a-plane position="3 0.5 0.1" src="https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1WM0VIZHFiZ2FhV2dwamRVZGFaNTIyck5mOTJyZ2s0OVRXd1ViSzk0cGRGbg==" rotation="0 0 0" width="1.5" height="2"></a-plane>
           <a-light type="point" color="blue" position="0 0.5 1" intensity="0.1"></a-light>
           <a-light type="point" color="red" position="3 0.5 1" intensity="0.05"></a-light>
           <a-light type="point" color="blue" position="-3 0.5 1" intensity="0.1"></a-light>
