88be61eb08c6b64afde6b2b74847a8ef6a1b3a9f
lemonadesquash <ruth.damayanti.sitanggang@gmail.com>
2023-05-10 16:35:42 +0700

    Styling: Adding web icon

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -49,7 +49,7 @@ export default function Navbar() {
         <div className={styles.container}>
           <div className={styles.navbarLeft}>
             <div className={styles.logo}>
-              <img src='/assets/cyberscape-logo.png'/>
+              <img src='/assets/cyberscape-logo-nav.png'/>
             </div>
           </div>
           <div className={styles.navbarRight}>
