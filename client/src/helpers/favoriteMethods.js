db7640a8bf43f0aa91a4f622354afdc9b601888a
Edward <edw26alex@protonmail.com>
2023-05-10 14:19:12 +0700

    fix: fix ratingMethods & favoriteMethods

--- client/src/helpers/favoriteMethods.js
+++ client/src/helpers/favoriteMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function addFavorite(id) {
-    return await axios.post(`${import.meta.env.VITE_SERVER_URL}/favorite/${id}`, {}, {
+    return await axios.post(`${import.meta.env.VITE_SERVER_URL}/favorites/${id}`, {}, {
         headers: {
             access_token: localStorage.access_token
         }
@@ -9,7 +9,7 @@ export async function addFavorite(id) {
 }
 
 export async function removeFavorite(id) {
-    return await axios.delete(`${import.meta.env.VITE_SERVER_URL}/favorite/${id}`, {}, {
+    return await axios.delete(`${import.meta.env.VITE_SERVER_URL}/favorites/${id}`, {}, {
         headers: {
             access_token: localStorage.access_token
         }
