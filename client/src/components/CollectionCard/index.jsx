ea0b38da37592a2e70124a4c521c395a16d4f670
Edward <edw26alex@protonmail.com>
2023-05-10 15:58:51 +0700

    feat: finished profile feature

--- client/src/components/CollectionCard/index.jsx
+++ client/src/components/CollectionCard/index.jsx
@@ -1,6 +1,14 @@
+import { useDispatch } from 'react-redux';
 import styles from './collectioncard.module.css'
+import { fetchUserAfterUnfavorited } from '../../actions/usersCreators';
 
 export default function CollectionCard(props) {
+    const dispatch = useDispatch();
+
+    function handleUnfavorite() {
+        dispatch(fetchUserAfterUnfavorited(props.id))
+    }
+
     return(
         <div className={styles.card}>
             <img src={props.imageUrl}  />
@@ -8,7 +16,14 @@ export default function CollectionCard(props) {
                 <h1>{props.title}</h1>
                 <p>{props.description}</p>
             </div>
-            {props.action && props.action}
+            { props.action && props.action }
+            <div className={styles.button}>
+                <button
+                className={styles.btn}
+                onClick={handleUnfavorite}>
+                    Unfavorite
+                </button>
+            </div>
         </div>
     )
 }
--- client/src/components/FavoriteList/index.jsx
+++ client/src/components/FavoriteList/index.jsx
@@ -1,15 +1,17 @@
 import styles from "./favoritelist.module.css";
 import CollectionCard from "../CollectionCard";
-import { fetchUserAfterQuotaReduce } from "../../actions/usersCreators";
+import { fetchUserAfterUnfavorited } from "../../actions/usersCreators";
 import { useDispatch, useSelector } from "react-redux";
 import ShareButtons from "@/components/ShareButtons";
 
 export default function CollectionList() {
   const arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   const profile = useSelector(state => state.user);
+  const { UserFavorites } = profile
 
-  return (
+  if (UserFavorites) return (
     <>
+      {/* { console.log(UserFavorites) } */}
       <div className={styles.favoriteList}>
         <div className={styles.container}>
           <div>
@@ -17,25 +19,24 @@ export default function CollectionList() {
           </div>
           <div className={styles.cards}>
             <div className="carousel carousel-end rounded-box">
-              {arr.map((el) => {
+              { UserFavorites.map(el => {
+                const { NFT } = el;
                 return (
                   <>
+                    {/* { console.log(NFT, '<- di dalam mapping nft')} */}
                     <div className="carousel-item ml-10">
                       <CollectionCard
-                        imageUrl="https://via.placeholder.com/200"
-                        title="NFT 1"
+                        imageUrl={ NFT.imageUrl }
+                        title={ NFT.title }
                         action={
                           <>
                             <ShareButtons
-                              title="Ini NFT 1"
-                              url="https://google.com/nft1"
+                              title={ `Check out this ${NFT.title} picture! Only in the https://cyberscape-p3.web.app` }
+                              url={ NFT.imageUrl }
                             />
-
-                            <div className={styles.button}>
-                              <button className={styles.btn}>Delete</button>
-                            </div>
                           </>
                         }
+                        id={ NFT.id }
                       />
                     </div>
                   </>
