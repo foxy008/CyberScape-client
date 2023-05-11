5424254c430040d2faa0a3b0f8c670573627b1a6
lemonadesquash <ruth.damayanti.sitanggang@gmail.com>
2023-05-11 10:13:34 +0700

    Styling: Top and Gallery Collections

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -4,31 +4,31 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../actions/usersCreators";
 import { getAllRooms } from "../actions/roomsCreators";
 import Room from "../components/Room";
+import GalaxyBackground from "../components/GalaxyBackground";
 
 export default function Gallery() {
   const dispatch = useDispatch();
-  const rooms = useSelector(state => state.rooms);
-  const profile = useSelector(state => state.user);
+  const rooms = useSelector((state) => state.rooms);
+  const profile = useSelector((state) => state.user);
 
   useEffect(() => {
-    dispatch(getAllRooms())
+    dispatch(getAllRooms());
 
     function handleKeyDown(event) {
       localStorage.access_token ? dispatch(fetchUser()) : null;
       if (event.code === "Space" && !localStorage.getItem("camera")) {
-        handleElevatorClick("up")
-        localStorage.setItem("camera", "up")
-      }
-      else if (event.code === "Space" && localStorage.getItem("camera")) {
-        handleElevatorClick("down")
-        localStorage.removeItem("camera")
+        handleElevatorClick("up");
+        localStorage.setItem("camera", "up");
+      } else if (event.code === "Space" && localStorage.getItem("camera")) {
+        handleElevatorClick("down");
+        localStorage.removeItem("camera");
       }
     }
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [])
+  }, []);
 
   function handleElevatorClick(view) {
     if (view === "up") {
@@ -36,105 +36,306 @@ export default function Gallery() {
     }
 
     if (view === "down") {
-      setCameraY(1.6)
+      setCameraY(1.6);
     }
   }
 
   const [cameraY, setCameraY] = useState(1.6);
   const elevatorRef = useRef(null);
   const positions = [
-    "0 0 -16"
-    , "0 0 0"
-    , "0 0 16"
-    , "0 0 32"
-    , "0 0 48"
-    , "0 7 -16"
-    , "0 7 0"
-    , "0 7 16"
-    , "0 7 32"
-    , "0 7 48"]
+    "0 0 -16",
+    "0 0 0",
+    "0 0 16",
+    "0 0 32",
+    "0 0 48",
+    "0 7 -16",
+    "0 7 0",
+    "0 7 16",
+    "0 7 32",
+    "0 7 48",
+  ];
 
-  return <>
-    {
-      !rooms ? null : (!rooms[2] ? null : console.log(rooms[2].RoomNFTs, '<-- rooms'))
-      // Buat cek apakah rooms dapet di fetch, uncomment kalo ngak mau console
-    }
-    {
-      !profile ? null : console.log(profile, '<-- profile')
-      // Buat cek apakah profile dapet di fetch, uncomment kalo ngak mau keliatan di console
-    }
-    <div style={{ fontSize: '50px', paddingTop: '100px', textAlign: 'center' }}>
-                Main Gallery
-            </div>
-    <div style={{ width: 'fit', height: '480px', marginTop: '50px', marginBottom: '50px', marginRight: '200px', marginLeft: '200px' }}>
-      <a-scene embedded loading-screen="dotsColor: red; backgroundColor: black">
-        <a-assets>
-          <img
-            id="wallTexture"
-            src="/assets/output.jpg" />
-          <img
-            id="star1"
-            src="/assets/pngwing.com (2).png" />
-          <img
-            id="heart1"
-            src="/assets/heart-png-38780.png" />
-        </a-assets>
-        <a-camera position={`22 ${cameraY} 16`} rotation="0 -90 0" wasd-controls-enabled="true">
-          <a-entity position="0 0 -1" geometry="primitive: sphere; radius: 0.01" material="color: #000000; shader: flat; opacity: 0.5">
-          </a-entity>
-        </a-camera>
-        <a-entity id="mouseCursor" cursor="rayOrigin: mouse"></a-entity>
-        <a-entity environment="preset: tron; fog: 0.8; dressingAmount: 50; seed: 300; playArea: 5"></a-entity>
+  return (
+    <>
+      <GalaxyBackground>
+        {
+          !rooms
+            ? null
+            : !rooms[2]
+            ? null
+            : console.log(rooms[2].RoomNFTs, "<-- rooms")
+          // Buat cek apakah rooms dapet di fetch, uncomment kalo ngak mau console
+        }
+        {
+          !profile ? null : console.log(profile, "<-- profile")
+          // Buat cek apakah profile dapet di fetch, uncomment kalo ngak mau keliatan di console
+        }
+        <div
+          style={{
+            fontSize: "50px",
+            paddingTop: "100px",
+            textAlign: "center",
+            fontFamily: "Orbitron",
+            color: "white",
+          }}
+        >
+          Main Gallery
+        </div>
+        <div
+          style={{
+            width: "fit",
+            height: "480px",
+            marginTop: "50px",
+            marginBottom: "50px",
+            marginRight: "200px",
+            marginLeft: "200px",
+          }}
+        >
+          <a-scene
+            embedded
+            loading-screen="dotsColor: red; backgroundColor: black"
+          >
+            <a-assets>
+              <img id="wallTexture" src="/assets/output.jpg" />
+              <img id="star1" src="/assets/pngwing.com (2).png" />
+              <img id="heart1" src="/assets/heart-png-38780.png" />
+            </a-assets>
+            <a-camera
+              position={`22 ${cameraY} 16`}
+              rotation="0 -90 0"
+              wasd-controls-enabled="true"
+            >
+              <a-entity
+                position="0 0 -1"
+                geometry="primitive: sphere; radius: 0.01"
+                material="color: #000000; shader: flat; opacity: 0.5"
+              ></a-entity>
+            </a-camera>
+            <a-entity id="mouseCursor" cursor="rayOrigin: mouse"></a-entity>
+            <a-entity environment="preset: tron; fog: 0.8; dressingAmount: 50; seed: 300; playArea: 5"></a-entity>
 
-        <a-box material="opacity: 0.5" rotation="0 90 0" position="23 7 16" width="80" height="14" depth="0.1"></a-box>
-        <a-box material="opacity: 0.5" rotation="0 90 0" position="15 7.5 37" width="38" height="1" depth="0.1"></a-box>
-        <a-box material="opacity: 0.5" rotation="0 90 0" position="15 7.5 -5" width="38" height="1" depth="0.1"></a-box>
-        <a-box material="opacity: 0.5" rotation="0 0 0" position="17 7 -24" width="12" height="14" depth="0.1"></a-box>
-        <a-box material="opacity: 0.5" rotation="0 0 0" position="17 7 56" width="12" height="14" depth="0.1"></a-box>
-        <a-box material="opacity: 0.5" rotation="0 0 0" position="17.5 7.5 18" width="5" height="1" depth="0.1"></a-box>
-        <a-box material="opacity: 0.5" rotation="0 0 0" position="17.5 7.5 14" width="5" height="1" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 0 0" position="21.5 7 18" width="3" height="14" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 0 0" position="21.5 7 14" width="3" height="14" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 90 0" position="20 5 16" width="4" height="4" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 90 0" position="20 12 16" width="4" height="4" depth="0.1"></a-box>
+            <a-box
+              material="opacity: 0.5"
+              rotation="0 90 0"
+              position="23 7 16"
+              width="80"
+              height="14"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              material="opacity: 0.5"
+              rotation="0 90 0"
+              position="15 7.5 37"
+              width="38"
+              height="1"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              material="opacity: 0.5"
+              rotation="0 90 0"
+              position="15 7.5 -5"
+              width="38"
+              height="1"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              material="opacity: 0.5"
+              rotation="0 0 0"
+              position="17 7 -24"
+              width="12"
+              height="14"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              material="opacity: 0.5"
+              rotation="0 0 0"
+              position="17 7 56"
+              width="12"
+              height="14"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              material="opacity: 0.5"
+              rotation="0 0 0"
+              position="17.5 7.5 18"
+              width="5"
+              height="1"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              material="opacity: 0.5"
+              rotation="0 0 0"
+              position="17.5 7.5 14"
+              width="5"
+              height="1"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 0 0"
+              position="21.5 7 18"
+              width="3"
+              height="14"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 0 0"
+              position="21.5 7 14"
+              width="3"
+              height="14"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 90 0"
+              position="20 5 16"
+              width="4"
+              height="4"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 90 0"
+              position="20 12 16"
+              width="4"
+              height="4"
+              depth="0.1"
+            ></a-box>
 
-        <a-box animation__1="property: position; to: 20 1.5 17.5; dur: 3000; easing: linear; startEvents: mouseleave;"
-          animation__2="property: position; to: 20 1.5 16.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;" rotation="0 90 0" position="20 1.5 16.5" width="1" height="3" depth="0.05"></a-box>
-        <a-box animation__1="property: position; to: 20 1.5 14.5; dur: 3000; easing: linear; startEvents: mouseleave;"
-          animation__2="property: position; to: 20 1.5 15.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;" rotation="0 90 0" position="20 1.5 15.5" width="1" height="3" depth="0.05"></a-box>
-        
-        <a-box src="#wallTexture" rotation="0 90 0" position="20 8.5 14.5" width="1" height="3" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 90 0" position="20 8.5 17.5" width="1" height="3" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 90 0" position="20 1.5 14.5" width="1" height="3" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 90 0" position="20 1.5 17.5" width="1" height="3" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="0 0 0" position="21.5 3 16" width="3" height="0.1" depth="4"></a-box>
-        <a-box src="#wallTexture" rotation="0 0 0" position="21.5 7 16" width="3" height="0.1" depth="4"></a-box>
-        <a-box src="#wallTexture" rotation="0 0 0" position="21.5 10 16" width="3" height="0.1" depth="4"></a-box>
+            <a-box
+              animation__1="property: position; to: 20 1.5 17.5; dur: 3000; easing: linear; startEvents: mouseleave;"
+              animation__2="property: position; to: 20 1.5 16.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;"
+              rotation="0 90 0"
+              position="20 1.5 16.5"
+              width="1"
+              height="3"
+              depth="0.05"
+            ></a-box>
+            <a-box
+              animation__1="property: position; to: 20 1.5 14.5; dur: 3000; easing: linear; startEvents: mouseleave;"
+              animation__2="property: position; to: 20 1.5 15.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;"
+              rotation="0 90 0"
+              position="20 1.5 15.5"
+              width="1"
+              height="3"
+              depth="0.05"
+            ></a-box>
 
-        <a-box animation__1="property: position; to: 20 8.5 17.5; dur: 3000; easing: linear; startEvents: mouseleave;"
-          animation__2="property: position; to: 20 8.5 16.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;" rotation="0 90 0" position="20 8.5 16.5" width="1" height="3" depth="0.05"></a-box>
-        <a-box animation__1="property: position; to: 20 8.5 14.5; dur: 3000; easing: linear; startEvents: mouseleave;"
-          animation__2="property: position; to: 20 8.5 15.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;" rotation="0 90 0" position="20 8.5 15.5" width="1" height="3" depth="0.05"></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 90 0"
+              position="20 8.5 14.5"
+              width="1"
+              height="3"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 90 0"
+              position="20 8.5 17.5"
+              width="1"
+              height="3"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 90 0"
+              position="20 1.5 14.5"
+              width="1"
+              height="3"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 90 0"
+              position="20 1.5 17.5"
+              width="1"
+              height="3"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 0 0"
+              position="21.5 3 16"
+              width="3"
+              height="0.1"
+              depth="4"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 0 0"
+              position="21.5 7 16"
+              width="3"
+              height="0.1"
+              depth="4"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="0 0 0"
+              position="21.5 10 16"
+              width="3"
+              height="0.1"
+              depth="4"
+            ></a-box>
 
-        <a-box src="#wallTexture" rotation="90 0 90" position="18 14 16" width="84" height="14" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="90 0 90" position="13 7 16" width="80" height="4" depth="0.1"></a-box>
-        <a-box src="#wallTexture" rotation="90 0 90" position="17.5 7 16" width="4" height="5" depth="0.1"></a-box>
+            <a-box
+              animation__1="property: position; to: 20 8.5 17.5; dur: 3000; easing: linear; startEvents: mouseleave;"
+              animation__2="property: position; to: 20 8.5 16.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;"
+              rotation="0 90 0"
+              position="20 8.5 16.5"
+              width="1"
+              height="3"
+              depth="0.05"
+            ></a-box>
+            <a-box
+              animation__1="property: position; to: 20 8.5 14.5; dur: 3000; easing: linear; startEvents: mouseleave;"
+              animation__2="property: position; to: 20 8.5 15.5; dur: 3000; easing: linear; startEvents: animationcomplete__1;"
+              rotation="0 90 0"
+              position="20 8.5 15.5"
+              width="1"
+              height="3"
+              depth="0.05"
+            ></a-box>
 
-        <a-box position="18 0 0" width="0.5" height="" depth="3"></a-box>
-        <a-box position="18 0 32" width="0.5" height="" depth="3"></a-box>
-        <a-box position="18 0 48" width="0.5" height="" depth="3"></a-box>
-        <a-box position="18 0 -16" width="0.5" height="" depth="3"></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="90 0 90"
+              position="18 14 16"
+              width="84"
+              height="14"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="90 0 90"
+              position="13 7 16"
+              width="80"
+              height="4"
+              depth="0.1"
+            ></a-box>
+            <a-box
+              src="#wallTexture"
+              rotation="90 0 90"
+              position="17.5 7 16"
+              width="4"
+              height="5"
+              depth="0.1"
+            ></a-box>
 
-        {rooms.map((el, i) => {
-          if (i < 10) {
-            return <Room position={positions[i]} nfts={rooms[i]} ></Room>
-          }
-        })}
+            <a-box position="18 0 0" width="0.5" height="" depth="3"></a-box>
+            <a-box position="18 0 32" width="0.5" height="" depth="3"></a-box>
+            <a-box position="18 0 48" width="0.5" height="" depth="3"></a-box>
+            <a-box position="18 0 -16" width="0.5" height="" depth="3"></a-box>
 
-      </a-scene>
-
-    </div>
-
-  </>
-
+            {rooms.map((el, i) => {
+              if (i < 10) {
+                return <Room position={positions[i]} nfts={rooms[i]}></Room>;
+              }
+            })}
+          </a-scene>
+        </div>
+      </GalaxyBackground>
+    </>
+  );
 }
